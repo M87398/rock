fbdf280d01a44b9e1fda83f53d0e7f8ff82a5ff3
Your Name <you@example.com>
2023-05-19 11:40:21 +0200

    initial commit

---
+++ src/test/java/rockwell/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package rockwell.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
